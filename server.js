3a0f79a230d63649498053777f323be382e4b3e5
ashtonrumaldo <ashtonrumaldo@gmail.com>
2025-11-03 14:03:23 -0500

    Added Users Routes

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log('Here');
-    res.render("index", { text: "World!" });
+    res.render("index", { user: "Ashton!" });
 }); //This function will run when someone goes to the root folder.
+
+app.get('/users', (req, res) => {
+    res.send('User List');
+});
+
+app.get('/users/new', (req, res) => {
+    res.send('New User Form');
+});
+
+
+
+
+
 
 app.listen(3030);
