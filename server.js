2ddc8f3eb3b7dee63fbf5095a0b50aada9eecbfc
ashtonrumaldo <ashtonrumaldo@gmail.com>
2025-11-03 13:45:48 -0500

    added EJS and added first HTML file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const app = express(); //Calling this function sets up a server.
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log('Here');
-    res.send("Hello!");
+    res.render("index", { text: "World!" });
 }); //This function will run when someone goes to the root folder.
 
 app.listen(3030);
