78eb0126daa7e030dcf8b9233b73fae88f3e536b
ashtonrumaldo <ashtonrumaldo@gmail.com>
2025-11-11 20:05:53 -0500

    fixed app crash error (case sensitive @ users.js, line 3; express.Router();)

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+const userRouter = require('./routes/users');
+
 const app = express(); //Calling this function sets up a server.
 app.set('view engine', 'ejs');
+app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
     console.log('Here');
     res.render("index", { user: "Ashton!" });
-}); //This function will run when someone goes to the root folder.
+});
 
 app.get('/users', (req, res) => {
     res.send('User List');
@@ -16,9 +19,3 @@ app.get('/users/new', (req, res) => {
     res.send('New User Form');
 });
 
-
-
-
-
-
-app.listen(3030);
